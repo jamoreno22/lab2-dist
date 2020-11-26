acc11efef9f83a4eb1c5dc4241dfcc543bf71d79
jamoreno22 <javier.morenog@sansano.usm.cl>
2020-11-26 18:03:35 -0300

    avance datanode

--- data/client.go
+++ data/client.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"math"
+	"os"
+	"strconv"
 
+	data "github.com/jamoreno22/lab2_dist/pkg/proto/DataNode"
 	"google.golang.org/grpc"
 )
 
@@ -13,6 +20,56 @@ func main() {
 		log.Fatalf("did not connect: %s", err)
 	}
 
-	log.Println("Client connected...")
 	defer conn.Close()
+
+	dc := data.NewDataNodeClient(conn)
+
+	fileToBeChunked := "../books/Moby_Dick-Herman_Melville.pdf"
+
+	file, err := os.Open(fileToBeChunked)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer file.Close()
+
+	fileInfo, _ := file.Stat()
+
+	var fileSize int64 = fileInfo.Size()
+
+	const fileChunk = 0.25 * (1 << 20) // 1 MB, change this to your requirement
+
+	// calculate total number of parts the file will be chunked into
+
+	totalPartsNum := uint64(math.Ceil(float64(fileSize) / float64(fileChunk)))
+
+	fmt.Printf("Splitting to %d pieces.\n", totalPartsNum)
+
+	for i := uint64(0); i < totalPartsNum; i++ {
+
+		partSize := int(math.Min(fileChunk, float64(fileSize-int64(i*fileChunk))))
+		partBuffer := make([]byte, partSize)
+
+		file.Read(partBuffer)
+
+		// write to disk
+		fileName := "somebigfile_" + strconv.FormatUint(i, 10)
+		_, err := os.Create(fileName)
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// write/save buffer to disk
+		ioutil.WriteFile(fileName, partBuffer, os.ModeAppend)
+
+		fmt.Println("Split to : ", fileName)
+	}
+
+	c.UploadBook(context.Background(), &)
+	log.Println("Client connected...")
+
 }
--- server/server.go
+++ server/server.go
@@ -5,12 +5,17 @@ import (
 	"log"
 	"net"
 
-	lab2 "github.com/jamoreno22/lab2_dist/pkg/proto/DataNode"
+	data "github.com/jamoreno22/lab2_dist/pkg/proto/DataNode"
+	name "github.com/jamoreno22/lab2_dist/pkg/proto/NameNode"
 	"google.golang.org/grpc"
 )
 
-type server struct {
-	lab2.UnimplementedDataNodeServer
+type dataServer struct {
+	data.UnimplementedDataNodeServer
+}
+
+type nameServer struct {
+	name.UnimplementedNameNodeServer
 }
 
 func main() {
@@ -19,12 +24,26 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	} // create a server instance
-	s := server{}                               // create a gRPC server object
-	grpcServer := grpc.NewServer()              // attach the Ping service to the server
-	lab2.RegisterDataNodeServer(grpcServer, &s) // start the server
+	ds := dataServer{}                               // create a gRPC server object
+	grpcDataServer := grpc.NewServer()               // attach the Ping service to the server
+	data.RegisterDataNodeServer(grpcDataServer, &ds) // start the server
 
 	log.Println("Server running ...")
-	if err := grpcServer.Serve(lis); err != nil {
+	if err := grpcDataServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+
+	// create a listener on TCP port 7777
+	namelis, err := net.Listen("tcp", fmt.Sprintf(":%d", 8000))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	} // create a server instance
+	ns := nameServer{}                               // create a gRPC server object
+	grpcNameServer := grpc.NewServer()               // attach the Ping service to the server
+	name.RegisterNameNodeServer(grpcNameServer, &ns) // start the server
+
+	log.Println("Server running ...")
+	if err := grpcNameServer.Serve(namelis); err != nil {
 		log.Fatalf("failed to serve: %s", err)
 	}
 }
